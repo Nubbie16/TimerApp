18c32fd2a3e976a60785f775849f2be4fb26f9aa
Cody Eckard <eckard.cody@gmail.com>
2026-05-18 13:38:42 -0400

    test

--- TimerApp/Form1.cs
+++ TimerApp/Form1.cs
@@ -1,8 +1,8 @@
 namespace TimerApp
 {
-    public partial class Form1 : Form
+    public partial class main : Form
     {
-        public Form1()
+        public main()
         {
             InitializeComponent();
         }
