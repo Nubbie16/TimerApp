ea511b4e4b8b0a342cabdb5e9f56ac332f278d97
Cody Eckard <eckard.cody@gmail.com>
2026-05-18 13:44:50 -0400

    Resize push test

--- TimerApp/Main.Designer.cs
+++ TimerApp/Main.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(354, 472);
             Name = "main";
             Text = "Timer";
             ResumeLayout(false);
